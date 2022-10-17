ba6207e2ae1fc1643c1c65ae4af23fd57b5927b5
José Guimarães <joseguimaraes96@hotmail.com>
2022-10-17 10:45:56 +0100

    Create EFCore extensions

---
+++ src/Core/SimpleStore.Core.EntityFrameworkCore/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleStore.Core.EntityFrameworkCore.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task<IApplicationBuilder> MigrateDatabaseAsync<TContext>(this IApplicationBuilder applicationBuilder)
+            where TContext : DbContext
+        {
+            using var scope = applicationBuilder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<TContext>();
+
+            await context.Database.MigrateAsync();
+
+            return applicationBuilder;
+        }
+
+        public static IApplicationBuilder MigrateDatabase<TContext>(this IApplicationBuilder applicationBuilder)
+           where TContext : DbContext
+        {
+            using var scope = applicationBuilder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<TContext>();
+
+            context.Database.Migrate();
+
+            return applicationBuilder;
+        }
+    }
+}
